0f7ae7b0c7912a36da18ad0b096b809a5ac10ead
venkipat <venki.pat@gmail.com>
2017-10-01 20:29:08 +0530

    Update Constants.groovy

--- src/jenkins/Constants.groovy
+++ src/jenkins/Constants.groovy
@@ -8,7 +8,7 @@ class Constants {
     static final SONAR_URL = 'enter sonarqube your url here';
     static final GITLAB_WEB_BASE_URL = 'enter gitlab url here';
     static final GITLAB_API_BASE_URL = 'enter gitlab api url here';
-    static final GITLAB_CHECKOUT_BASE_URL = 'enter gitlab checkout url here';
+    static final GITLAB_CHECKOUT_BASE_URL = 'https://github.com/venkipat/';
     static final GITLAB_VERSION = '8.6';
     static final GITLAB_CREDENTIALS_ID = 'enter credentials here';
     static final GITLAB_API_TOKEN_CREDENTIALS_ID = 'enter api token here';
